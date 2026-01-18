f8e29e42cc4d7afbd182a29226eb8f77f090a388
almacode1703 <testgitt.2023@gmail.com>
2026-01-19 01:09:46 +0530

    Further Commits

---
+++ m4_l6/script.js
@@ -0,0 +1,10 @@
+function addNums(a,b){
+    return a+b
+}
+
+function displaySum(){
+    result = addNums(5,6)
+    document.getElementById("demo").innerHTML = result
+    document.getElementById("demo").classList.add('styleResult') = result
+
+}
